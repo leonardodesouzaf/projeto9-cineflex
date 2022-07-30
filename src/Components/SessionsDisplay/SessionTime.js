d224863c68839f39e258c396b14b3a010a7b3c37
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-07-30 19:40:00 -0300

    link to seats display done

--- src/Components/SessionsDisplay/SessionTime.js
+++ src/Components/SessionsDisplay/SessionTime.js
@@ -1,13 +1,12 @@
 import "./style.css";
 import { Link } from "react-router-dom";
 export default function SessionTime(props){
-    idRoute = `/assentos/${props.id}`;
+    let idRoute = `/assentos/${props.id}`;
     return(
         <>
-            <Link to={idRoute}></Link>
-            <div className="session-time">
-                {props.showtime}
-            </div>
+            <Link to={idRoute} className="session-time">
+                    {props.showtime}
+            </Link>
         </>
     )
 }
